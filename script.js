8378ca0f791ca433570640eb937c0186954e6c95
Dheeraj Surakasula <116566082+Dheeerazzz@users.noreply.github.com>
2022-11-22 06:55:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,11 @@ scissor=document.getElementById("scissor")
 printer=document.getElementById("printer")
 comse=document.getElementById("comse")
 comsi=document.getElementById("comsi")
+uscore=document.getElementById("userscore")
+cscore=document.getElementById("compscore")
+rest=document.getElementById("reset")
+user=0
+comp=0
 function compchoice(){
 
     const list=['rock','paper','scissor']
@@ -14,23 +19,37 @@ function compchoice(){
 
      return choice
      }
+function reset(){
+   user=0;
+   comp=0;
+   uscore.innerHTML="Your Score :  "+user;
+   cscore.innerHTML="Computer score :  "+comp;
+}
 function rocked(){
    krik=compchoice()
    console.log(krik)
    if(krik=="paper"){
       comsi.innerHTML="you chose ROCK"
       comse.innerHTML="computer chose PAPER"
-       printer.innerHTML="You Lose &#x1F62D"
+      printer.innerHTML=">> You Lose &#x1F62D <<"
+       comp=comp+1
+       uscore.innerHTML="Your Score :  "+user;
+       cscore.innerHTML="Computer score :  "+comp;
    }
    else if(krik=="scissor"){
       comsi.innerHTML="you chose ROCK"
        comse.innerHTML="computer chose SCISSORS"
-       printer.innerHTML="You Win!&#x1F60E"
+       printer.innerHTML=">> You Win!&#x1F60E <<"
+       user=user+1
+       uscore.innerHTML="Your Score :  "+user;
+       cscore.innerHTML="Computer score :  "+comp;
    }
    else {
       comsi.innerHTML="you chose ROCK"
       comse.innerHTML="computer chose ROCK"
-      printer.innerHTML="It's a Tie &#x1F927"
+      printer.innerHTML=">> It's a Tie &#x1F927 <<"
+      uscore.innerHTML="Your Score :  "+user;
+      cscore.innerHTML="Computer score :  "+comp;
    }
 }
 function papered(){
@@ -39,17 +58,25 @@ function papered(){
    if(krik=="paper"){
       comsi.innerHTML="you chose PAPER"
        comse.innerHTML="computer chose PAPER"
-       printer.innerHTML="It's a Tie &#x1F927"
+       printer.innerHTML=">> It's a Tie &#x1F927 <<"
+       uscore.innerHTML="Your Score :  "+user;
+       cscore.innerHTML="Computer score :  "+comp;
    }
    else if(krik=="scissor"){
       comsi.innerHTML="you chose PAPER"
        comse.innerHTML="computer chose SCISSORS"
-       printer.innerHTML="You Lose &#x1F62D"
+       printer.innerHTML=">> You Lose &#x1F62D <<"
+       comp=comp+1
+       uscore.innerHTML="Your Score :  "+user;
+       cscore.innerHTML="Computer score :  "+comp;
    }
    else {
       comsi.innerHTML="you chose PAPER"
       comse.innerHTML="computer chose ROCK"
-      printer.innerHTML="You Win!&#x1F60E"
+      printer.innerHTML=">> You Win!&#x1F60E <<"
+      user=user+1
+      uscore.innerHTML="Your Score :  "+user;
+      cscore.innerHTML="Computer score :  "+comp;
    }
 }
 function scissored(){
@@ -58,17 +85,25 @@ function scissored(){
    if(krik=="paper"){
       comsi.innerHTML="you chose SCISSORS"
        comse.innerHTML="computer chose PAPER"
-       printer.innerHTML="You Win!&#x1F60E"
+       printer.innerHTML=">> You Win!&#x1F60E <<"
+       user=user+1
+       uscore.innerHTML="Your Score :  "+user;
+       cscore.innerHTML="Computer score :  "+comp;
    }
    else if(krik=="scissor"){
       comsi.innerHTML="you chose SCISSORS"
        comse.innerHTML="computer chose SCISSORS"
-       printer.innerHTML="It's a Tie &#x1F927"
+       printer.innerHTML=">> It's a Tie &#x1F927 <<"
+       uscore.innerHTML="Your Score :  "+user;
+       cscore.innerHTML="Computer score :  "+comp;
    }
    else {
       comsi.innerHTML="you chose SCISSORS"
       comse.innerHTML="computer chose ROCK"
-      printer.innerHTML="You Lose &#x1F62D"
+      printer.innerHTML=">> You Lose &#x1F62D <<"
+      comp=comp+1
+      uscore.innerHTML="Your Score :  "+user;
+      cscore.innerHTML="Computer score :  "+comp;
    }
 }
     
